c3285118dcd08ba0fa3642eefbe569cd51378457
deesampan <76961599+deesampan@users.noreply.github.com>
2024-01-30 22:27:32 +0700

    fixed search page

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-    res.render("customer/customer_search.ejs");
-})
+    res.render("main_page.ejs");
+});
 
 app.get("/login",(req,res)=>{
     res.render("login_page.ejs");
-})
+});
 
 app.get("/regis/customer",(req,res)=>{
     res.render("regis_customer.ejs");
-})
+});
 
 app.get("/regis/landlord",(req,res)=>{
     res.render("regis_landlord.ejs");
-})
+});
 
 app.listen(port,()=>{
     console.log("server is work!");
-})
+});
