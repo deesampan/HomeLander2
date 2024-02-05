28ad37e5b457fadd6c4e41c0e856f56e40864ff0
Jilamika <tartomgt@gmail.com>
2024-02-05 22:55:40 +0700

    add landlord main page

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/",(req,res)=>{
     res.render("main_page.ejs");
 })
 
+app.get("/landlord",(req,res)=>{
+    res.render("landlord/landlord_main.ejs");
+})
+
 app.listen(port,()=>{
     console.log("server is work!");
 })
